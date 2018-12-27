62ed7142ad7664dd1b08e7a6fddc8d34de3bb400
itsZens <damura@msi-damura>
2018-12-27 12:00:31 +0100

    Creació encaminament app.module.ts (Routing)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { AppComponent } from './FavouritesList.component';
+import { AppComponent } from './TVSeriesList.component';
+import { AppComponent } from './MoviesList.component';
+
+
+
+const appRoutes: Routes = [
+                           { path: '', component: AppComponent },
+                           { path: 'favourites', component: FavouritesListComponent },
+                           { path: 'tvseries', component: TVSeriesListComponent},
+                           { path: 'movies', component: MoviesListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavouritesListComponent,
+    TVSeriesListComponent,
+    MoviesListComponent
   ],
   imports: [
     BrowserModule,
