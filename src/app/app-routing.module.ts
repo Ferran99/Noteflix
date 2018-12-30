32764351642ad3012cbe606603d78756127e78b2
ferran <ferran@ferran-GE62VR-6RF>
2018-12-30 14:14:32 +0100

    Router link fet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FavouritesComponent } from 'src/app/favourites/favourites.component';
 
-const routes: Routes = [];
+import { MoviesComponent } from 'src/app/movies/movies.component';
+import { HomeComponent } from "src/app/home/home.component";
+
+const routes: Routes = [
+                        { path: '', redirectTo: '/home', pathMatch: 'full' },
+                        { path: 'home', component: HomeComponent },
+                        { path: 'movies', component: MoviesComponent },
+                        { path: 'favourites', component: FavouritesComponent }
+
+                       /* { path: 'tvseries', component: TVSeriesListComponent},
+                        { path: 'movies', component: MoviesListComponent}
+*/];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router';
 
+
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { HomeComponent } from './home/home.component';
+import { FavouritesComponent } from './favourites/favourites.component';
 /*import { AppComponent } from './FavouritesList.component';
 import { AppComponent } from './TVSeriesList.component';
 import { AppComponent } from './MoviesList.component';
 
+*/
 
-
-const appRoutes: Routes = [
-                           { path: '', component: AppComponent },
-                           { path: 'favourites', component: FavouritesListComponent },
-                           { path: 'tvseries', component: TVSeriesListComponent},
-                           { path: 'movies', component: MoviesListComponent}
-];*/
 
 @NgModule({
   declarations: [
     AppComponent,
+    MoviesComponent,
+    HomeComponent,
+    FavouritesComponent,
     /*FavouritesListComponent,
     TVSeriesListComponent,
     MoviesListComponent*/
