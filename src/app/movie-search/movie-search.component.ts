fef91ddaf64e73f8e5dd20b56ee9be5edf624bf2
ferran <ferran@ferran-XPS-13-9370>
2019-01-08 16:13:43 +0100

    Modify show movies on search movies

--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -2,8 +2,8 @@ import { MovieService } from '../movie.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { ElementRef } from '@angular/core';
 
-import { Movie } from "src/app/Movies";
-import { Observable, Subject } from "rxjs";
+import { Movie } from 'src/app/Movies';
+import { Observable, Subject } from 'rxjs';
 import {Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ReplaySubject } from 'rxjs';
@@ -17,19 +17,19 @@ import { take, takeUntil } from 'rxjs/operators';
   styleUrls: ['./movie-search.component.css']
 })
 export class MovieSearchComponent implements OnInit, OnDestroy {
-    
-    @ViewChild("arrayMovies") arrayMovies: ElementRef;
-    @ViewChild("arrayMoviesSearch") arrayMoviesSearch: ElementRef;
 
-    
-    
+    @ViewChild('arrayMovies') arrayMovies: ElementRef;
+    @ViewChild('arrayMoviesSearch') arrayMoviesSearch: ElementRef;
+
+
+
     movies: Movie[];
     /*movies$: Observable<Movie[]>;
     private searchTerms = new Subject<string>();*/
-    
+
     /** control for the selected movie */
     public movieCtrl: FormControl = new FormControl();
-    
+
     /** control for the MatSelect filter keyword */
     public movieFilterCtrl: FormControl = new FormControl();
 
@@ -38,7 +38,7 @@ export class MovieSearchComponent implements OnInit, OnDestroy {
 
     /** control for the MatSelect filter keyword multi-selection */
     public movieMultiFilterCtrl: FormControl = new FormControl();
-    
+
 
     /** list of Movies filtered by search keyword */
     public filteredMovies: ReplaySubject<Movie[]> = new ReplaySubject<Movie[]>(1);
@@ -47,15 +47,15 @@ export class MovieSearchComponent implements OnInit, OnDestroy {
 
     /** Subject that emits when the component has been destroyed. */
     private _onDestroy = new Subject<void>();
-    
+
     constructor(private movieService: MovieService) { }
 
     /*search(term: string): void {
         this.searchTerms.next(term);
-      }*/   
+      }*/
   ngOnInit() {
       this.getMovies();
-      
+
    // load the initial movie list
       this.filteredMovies.next(this.movies.slice());
 
@@ -65,7 +65,7 @@ export class MovieSearchComponent implements OnInit, OnDestroy {
         .subscribe(() => {
           this.filterMovies();
         });
-     
+
   }
   ngOnDestroy() {
       this._onDestroy.next();
@@ -76,7 +76,7 @@ export class MovieSearchComponent implements OnInit, OnDestroy {
       this.movieService.getMovies()
       .subscribe(movies => this.movies = movies);
     }
-  
+
   private filterMovies() {
       if (!this.movies) {
         return;
@@ -94,21 +94,21 @@ export class MovieSearchComponent implements OnInit, OnDestroy {
         this.movies.filter(movie => movie.title.toLowerCase().indexOf(search) > -1)
       );
     }
-  
-  movieShow(){
-      
+
+  movieShow() {
+
       this.arrayMoviesSearch.nativeElement.classList.remove('display-none');
       this.arrayMovies.nativeElement.classList.add('display-none');
 
-      
+
   }
-  movieShowAll(){
+  movieShowAll() {
       this.arrayMoviesSearch.nativeElement.classList.add('display-none');
       this.arrayMovies.nativeElement.classList.remove('display-none');
-      
-      
+
+
   }
-  
-  
+
+
 
 }
