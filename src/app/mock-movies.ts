1cd42f98cb7f98a04d96601725086116cece4d76
itsZens <danimr1999@gmail.com>
2019-01-08 16:27:19 +0100

    Retoques displaying movies

--- src/app/mock-movies.ts
+++ src/app/mock-movies.ts
@@ -2,11 +2,11 @@ import { Movie } from './Movies';
 
 export const MOVIES: Movie[] = [
 
- { title: "AQUAMAN",director: "James Wan", genre: "Science fiction", url: "https://daw2-ferran-castane.000webhostapp.com/Pelis/Aquaman-OfficialTrailer1.mp4" },
- { title: "BOHEMIAN RHAPSODY",director: "Bryan Singer", genre: "Biography", url: "https://daw2-ferran-castane.000webhostapp.com/Pelis/BohemianRhapsody-OfficialTrailer[HD]-20thCenturyFOX.mp4" },
- { title: "SUPERLÓPEZ",director: "Javier Ruiz Caldera", genre: "Comedy", url: "https://daw2-ferran-castane.000webhostapp.com/Pelis/SUPERL%C3%93PEZTr%C3%A1ilerEspa%C3%B1ol2(2018)Comedia,Aventura.mp4"},
+ { title: "AQUAMAN", director: "James Wan", genre: "Science fiction", url: "https://daw2-ferran-castane.000webhostapp.com/Pelis/Aquaman-OfficialTrailer1.mp4" },
+ { title: "BOHEMIAN RHAPSODY", director: "Bryan Singer", genre: "Biography", url: "https://daw2-ferran-castane.000webhostapp.com/Pelis/BohemianRhapsody-OfficialTrailer[HD]-20thCenturyFOX.mp4" },
+ { title: "SUPERLÓPEZ", director: "Javier Ruiz Caldera", genre: "Comedy", url: "https://daw2-ferran-castane.000webhostapp.com/Pelis/SUPERL%C3%93PEZTr%C3%A1ilerEspa%C3%B1ol2(2018)Comedia,Aventura.mp4"},
  { title: "Los crímenes de Grindelwald", director: "David Yates",  genre: "Fantasy", url: "https://daw2-ferran-castane.000webhostapp.com/Pelis/AnimlesFantasticos-LosCrimenesdeGrindelwald-TrailerOficialComicCon.mp4"},
- { title: "Hola",director: "Javir", genre: "Comedy", url: "https://daw2-ferran-castane.000webhostapp.com/Pelis/SUPERL%C3%93PEZTr%C3%A1ilerEspa%C3%B1ol2(2018)Comedia,Aventura.mp4"},
+ { title: "Hola", director: "Javir", genre: "Comedy", url: "https://daw2-ferran-castane.000webhostapp.com/Pelis/SUPERL%C3%93PEZTr%C3%A1ilerEspa%C3%B1ol2(2018)Comedia,Aventura.mp4"},
 
 
                                 
